c18b0143fa0344899def8bf0b8912eeb064c76dc
Bryan <bryannsanchez1234@gmail.com.com>
2022-04-15 09:00:49 -0700

    fixed errs

--- work_track/src/services/Auth.js
+++ work_track/src/services/Auth.js
@@ -13,7 +13,7 @@ export const SignInUser = async (data) => {
 
 export const RegisterUser = async (data) => {
   try {
-    const res = await Client.post('/auth/session')
+    const res = await Client.post('/auth/register', data)
     return res.data
   } catch (error) {
     throw error
