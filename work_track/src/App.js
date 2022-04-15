a7787ea0b0c1a49670ef51d4cec6234951e5e5b5
Bryan <bryannsanchez1234@gmail.com.com>
2022-04-15 08:41:55 -0700

    added register page

--- work_track/src/App.js
+++ work_track/src/App.js
@@ -1,8 +1,17 @@
+import { useState } from 'react'
 import './styles/App.css'
 import { Route, Routes } from 'react-router-dom'
 import Nav from './components/Nav'
 
 function App() {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+
+
+
+
+
   return (
     <div className="App">
       {/* <Nav/ > */}
