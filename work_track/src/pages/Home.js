1eb0fd93ef1738694e528328bea57f9c6ead5eb5
Frank Marra <frankrmarra@gmail.com>
2022-04-21 16:56:49 -0400

    more styling

--- work_track/src/pages/Home.js
+++ work_track/src/pages/Home.js
@@ -1,4 +1,5 @@
 import SignIn from './SignIn'
+import { Link } from 'react-router-dom'
 
 const Home = ({ setUser, toggleAuthenticated, user, authenticated }) => {
   return (
@@ -23,6 +24,13 @@ const Home = ({ setUser, toggleAuthenticated, user, authenticated }) => {
         <div className="site-info-wrapper">
           <p>Welcome to WorkTrack</p>
           <p>A simple time management app</p>
+
+          <p>
+            New User?
+            <span className="register-link">
+              <Link to="/register">Register</Link>
+            </span>
+          </p>
         </div>
       </div>
     </div>
