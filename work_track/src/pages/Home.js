f5e17e8c7ee7d673fe9e0faaaec82c78a3131c6f
Frank Marra <frankrmarra@gmail.com>
2022-04-21 20:43:09 -0400

    made mobile view of business view page

--- work_track/src/pages/Home.js
+++ work_track/src/pages/Home.js
@@ -1,7 +1,13 @@
 import SignIn from './SignIn'
 import { Link } from 'react-router-dom'
 
-const Home = ({ setUser, toggleAuthenticated, user, authenticated }) => {
+const Home = ({
+  setUser,
+  toggleAuthenticated,
+  user,
+  authenticated,
+  setOwnerId
+}) => {
   return (
     <div className="home-wrapper">
       <div className="site-title-wrapper">
--- work_track/src/pages/SignIn.js
+++ work_track/src/pages/SignIn.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { SignInUser } from '../services/Auth'
 import { useNavigate } from 'react-router-dom'
 
-const SignIn = ({ setUser, toggleAuthenticated, user }) => {
+const SignIn = ({ setUser, toggleAuthenticated, setOwnerId }) => {
   let navigate = useNavigate()
   const [formValues, setFormValues] = useState({
     ownerEmail: '',
